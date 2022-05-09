93c479ea75631de1c902f37ec43c2e95d93dbe74
hexaalif <hexaalif2020@gamil.com>
2022-05-10 01:49:52 +0600

    68.6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
@@ -21,7 +22,18 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("order");
 
+    // auth
+    app.get("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
+    // Service API
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -39,7 +51,25 @@ async function run() {
     // POST
     app.post("/service", async (req, res) => {
       const newService = req.body;
+      console.log(newService);
       const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // order collection API
+
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      console.log({ order });
+      const result = await orderCollection.insertOne(order);
       res.send(result);
     });
 
